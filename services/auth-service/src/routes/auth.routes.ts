d10cf168db95569d681e477ebc75859f264c9c90
muratozturk4u <muratozturk.cse@gmail.com>
2026-03-28 17:30:17 +0300

    [feat] feat: auth service route tanimlamalari

--- services/auth-service/src/routes/auth.routes.ts
+++ services/auth-service/src/routes/auth.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import { SignOptions } from 'jsonwebtoken';
 import { AuthController } from '../controllers/auth.controller';
+import { AuthService } from '../services/auth.service';
+import { UserRepository } from '../repositories/user.repository';
+import { UserModel } from '../models/user.model';
+import { Config } from '../config';
+
+const userRepository = new UserRepository(UserModel);
+const authService = new AuthService(userRepository, Config.JWT_SECRET, Config.JWT_EXPIRES_IN as SignOptions['expiresIn']);
+const authController = new AuthController(authService);
 
 const router = Router();
-const authController = new AuthController();
 
 router.post('/auth/register', authController.register.bind(authController));
 router.post('/auth/login', authController.login.bind(authController));
